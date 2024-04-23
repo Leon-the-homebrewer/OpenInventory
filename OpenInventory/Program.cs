fa3da6217f5e0b4d6378c652a27bebaeca482b9f
Leon Goodwin <leongoodwin@Michaels-MacBook-Pro.local>
2024-04-23 19:07:06 -0400

    Inital Commit

---
+++ OpenInventory/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter your username: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter your password: ");
+        string pass = Console.ReadLine();
+
+        Console.WriteLine("========================================");
+        Console.WriteLine("Welcome To OpenInventory Version 1.0.0");
+        Console.WriteLine("Logged in as " + name);
+        Console.WriteLine("========================================");
+    }
+}
